5a67812a53fc9f5df15287973a1b150dc17453b2
mdFuadIslam <fuadislamofficial@gmail.com>
2025-03-01 21:23:45 +0600

    js completed

--- script.js
+++ script.js
@@ -5,3 +5,46 @@ function backToHome() {
 function gotoQA() {
     window.location.href = "qa.html";
 }
+
+function completeTask(taskId, btnId) {
+    var task = document.getElementById(taskId).innerText;
+    console.log(task);
+    var completedTasks = document.getElementById("completed-tasks").innerText;
+    var remainingTasks = document.getElementById("remaining-tasks").innerText;
+    completedTasks = parseInt(completedTasks);
+    remainingTasks  = parseInt(remainingTasks);
+
+    document.getElementById("completed-tasks").innerText = (completedTasks + 1).toString();
+    document.getElementById("remaining-tasks").innerText = (remainingTasks - 1).toString();
+
+    const activity = document.createElement("div");
+    activity.className = "flex flex-col items-center justify-center p-2 m-4 gap-4 bg-slate-200 rounded-2xl";
+    const currentTime = new Date().toLocaleTimeString();
+    activity.textContent = 'You have completed the task ' + task + 'at ' + currentTime;
+    document.getElementById("activities").appendChild(activity);
+
+    alert("Board updated successfully!");
+    if (remainingTasks == 1) {
+        alert("Congratz! You have completed all the current tasks!");
+    }
+
+    document.getElementById(btnId).disabled = true;
+    
+}
+
+function clearHistory() {
+    document.getElementById("activities").innerHTML = "";
+}
+
+function themeChange() {
+    var color = "#"+((Math.random()*0xffffff)<<0).toString(16);
+    document.body.style.backgroundColor = color;
+
+}
+
+const date = new Date();
+const day = date.toLocaleString('default', { weekday: 'short' });
+const dateNum = date.toLocaleString('default', { day: '2-digit', month: '2-digit', year: '2-digit' });
+console.log(day, dateNum);
+document.getElementById("day").innerText = day+',';
+document.getElementById("date").innerText = dateNum;
